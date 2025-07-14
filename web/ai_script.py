2315c5e65ecd709fdcebb542bcb431e333143278
이준혁 <leehyuk0618@naver.com>
2025-07-14 15:28:57 +0900

    [GRAPH] ai_script.py 진행상황 세이브

--- web/ai_script.py
+++ web/ai_script.py
@@ -44,20 +44,21 @@ def detect_anomalies(file_path, exclude_columns=None):
     model = create_model('iforest')
     results = assign_model(model, score=True)
 
-    model_path = file_path.replace('.csv', '_model.pkl')
+    # 6. SHAP 그래프를 그리기 위한 파일 생성(1)
+    model_path = file_path.replace('.csv', '_model.pkl')    # SHAP값 계산을 위해 모델을 pkl파일로 추출
     joblib.dump(model, model_path)
-    shap_input_path = file_path.replace('.csv', '_X_for_shap.csv')
+    shap_input_path = file_path.replace('.csv', '_X_for_shap.csv')  # SHAP값 계산을 위해 실제 탐지 모델에 입력값으로 넣었던 data_scaled를 _X_for_shap.csv파일로 저장
     data_scaled.to_csv(shap_input_path, index=False)
 
-    # 6. 탐지 개수 집계
+    # 7. 탐지 개수 집계
     count_anomaly = int(results['Anomaly'].sum())
     total         = len(results)
 
-    # 7. 이상 탐지된 항목만 추출
+    # 8. 이상 탐지된 항목만 추출
     detected = results[results['Anomaly'] == 1]
     detected.to_csv("pycaret_detected_anomalies.csv", index=False)
 
- # 8. 결과를 HTML 테이블 + 요약 문자열로 반환
+    # 9. 결과를 HTML 테이블 + 요약 문자열로 반환
     result = {
         "summary": f"📌 이상치 {count_anomaly:,}건 / 전체 {total:,}건",
         "anomaly_count": int(count_anomaly),
@@ -65,6 +66,8 @@ def detect_anomalies(file_path, exclude_columns=None):
         # ★ 이상치(Anomaly==1)만 표로 보여줌
         "table_html": detected.to_html(index=False, classes="table table-sm") if count_anomaly > 0 else "<p>이상치가 없습니다.</p>",
     }
-    shap_values = shap.TreeExplainer(model).shap_values(data_scaled)
-    np.save(file_path.replace(".csv", "_shap_values.npy"), shap_values)
+
+    # 10. SHAP 그래프를 그리기 위한 파일 생성(2)
+    shap_values = shap.TreeExplainer(model).shap_values(data_scaled)    # 앞서 추출했던 모델, 입력값 파일을 가지고 shap_values를 계산해냄
+    np.save(file_path.replace(".csv", "_shap_values.npy"), shap_values) # 빠른 동작을 위해 미리 결과값을 .npy 파일로 저장한 뒤, ROW 행을 클릭할때마다 값을 꺼내 보여줌
     return result
